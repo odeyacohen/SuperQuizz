177cd50368670b58aeaedf4ee18b2e6f0e83bbc7
odeyacohen <odeyacohen05@gmail.com>
2023-10-27 12:05:07 +0200

    premier commit

---
+++ app/src/main/java/fr/euroforma/superquizz/WelcomeFragment.java
@@ -0,0 +1,75 @@
+package fr.euroforma.superquizz;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.euroforma.superquizz.databinding.FragmentWelcomeBinding;
+
+public class WelcomeFragment extends Fragment {
+    FragmentWelcomeBinding binding;
+
+    public static WelcomeFragment newInstance() {
+        WelcomeFragment fragment = new WelcomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.playButton.setEnabled(false);
+        binding.usernameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+        binding.playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Odeya", "click !");
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                QuizFragment quizFragment = new QuizFragment();
+                fragmentTransaction.replace(R.id.fragmentContainerView, quizFragment);
+                fragmentTransaction.commit();
+
+            }
+        });
+    }
+
+}
